fae713a0b283c6f1559cbbbd7df3d5827b020223
xlncrulz <99449070+xlncrulz@users.noreply.github.com>
2022-10-15 14:59:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function take_snapshot()
 
 console.log('ml5 version:',ml5.version);
 
-classifier =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KIUOd0Lts/',modelLoaded);
+classifier =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KIUOd0Lts/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log('Model Loaded!');
